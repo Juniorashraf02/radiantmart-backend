b3493da219eac5f7927701230ed1e7fcad07bd6f
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-08-24 16:58:02 +0600

    userschema file changed

--- models/userModel.js
+++ models/userModel.js
@@ -1,64 +1,79 @@
 const mongoose = require('mongoose'); // Erase if already required
+const bcrypt = require("bcrypt")
+
+
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:[true, "first name is required"],
+    firstName: {
+        type: String,
+        required: [true, "first name is required"],
         validate: {
-            validator: function (name){
-                return name.length>=3 && name.length<=8
+            validator: function (name) {
+                return name.length >= 3 && name.length <= 8
             },
             message: "first name must have 3 letters and less than 8 letters"
         },
-        index:true,
+        index: true,
     },
-    lastName:{
-        type:String,
-        required:[true, "last name is required"],
+    lastName: {
+        type: String,
+        required: [true, "last name is required"],
         validate: {
-            validator: function (name){
-                return name.length>=3 && name.length<=8
+            validator: function (name) {
+                return name.length >= 3 && name.length <= 8
             },
             message: "last name must have 3 letters and less than 8 letters"
         },
-        index:true,
+        index: true,
     },
-    
-    email:{
-        type:String,
-        required:[true, "email is required"],
-        validate:{
-            validator: function (email){
+
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        validate: {
+            validator: function (email) {
                 return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
             },
-            message: (props)=> `${props.email} is not valid email!`
+            message: (props) => `${props.email} is not valid email!`
         },
-        unique:true,
+        unique: true,
     },
-    mobile:{
+    mobile: {
         type: String,
-        required:[true, "mobile number is required"],
+        required: [true, "mobile number is required"],
         validate: {
-            validator: function (mobile){
-                return mobile.length>=10 
+            validator: function (mobile) {
+                return mobile.length >= 10
             },
             message: "must be atleast 11 digit"
         },
-        unique:true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:[true, "password is required"],
+    password: {
+        type: String,
+        required: [true, "password is required"],
         validate: {
-            validator: function (password){
-                return password.length>=5
+            validator: function (password) {
+                return password.length >= 5
             },
             message: "password must be atleast 5 letters!"
         },
         unique: true,
     },
 });
+
+
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
